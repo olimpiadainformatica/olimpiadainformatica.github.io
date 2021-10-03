cc13fe073ef68cef9dd98d8121efab839255473b
Jurius07 <f.ezequielmansilla@gmail.com>
2021-10-03 19:06:52 -0300

    Agregada las funciones de datos de usuario, y ligero diseño css a esta

--- datos_clinicos_datos.php
+++ datos_clinicos_datos.php
@@ -8,6 +8,62 @@
         header("location:login.php");
     }
 
+    $sesion=$_SESSION['dni_logeado'];
+    $consulta = "SELECT * FROM `u_direccion` WHERE dni_dir=$sesion;";
+    $sel = $con -> query($consulta);
+    $sel = mysqli_num_rows($sel);
+
+    if($sel != 1){
+        $sesion = $_SESSION['dni_logeado'];
+        $insercion = "INSERT INTO `u_direccion` (
+            `id_direccion`,
+            `calle_numero`,
+            `localidad`,
+            `codigo_postal`,
+            `provincia`,
+            `dni_dir`
+            ) VALUES (NULL, NULL, NULL, NULL, NULL, $sesion);";
+        $ins = $con -> query($insercion);
+    }
+
+    if(isset($_POST['edicion'])){
+        $editar="habilitado";
+    }
+
+    if(isset($_POST['guardado'])){
+        $apellido=$_POST['apellido'];
+        $nombre=$_POST['nombre'];
+        $f_nacimiento=$_POST['f_nacimiento'];
+        $sexo=$_POST['sexo'];
+        $email=$_POST['email'];
+        $n_s_social=$_POST['n_s_social'];
+        $direccion=$_POST['direccion'];
+        $localidad=$_POST['localidad'];
+        $c_postal=$_POST['c_postal'];
+        $provincia=$_POST['provincia'];
+        
+        $sesion = $_SESSION['dni_logeado'];
+        $actualizar_u = "UPDATE `usuario` SET
+            `apellido`='$apellido',
+            `nombre`='$nombre',
+            `correo`='$email',
+            `sexo`='$sexo',
+            `f_nacimiento`='$f_nacimiento',
+            `n_s_social`= $n_s_social
+        WHERE dni=$sesion;";
+        $ins_u = $con -> query($actualizar_u);
+
+        $actualizar_u_d = "UPDATE `u_direccion` SET
+            `calle_numero`='$direccion',
+            `localidad`='$localidad',
+            `codigo_postal`=$c_postal,
+            `provincia`='$provincia'
+        WHERE dni_dir=$sesion;";
+        $ins_u_d = $con -> query($actualizar_u_d);
+
+        $editar='';
+    }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,82 +82,120 @@
         <?php $dni = $_SESSION['dni_logeado']; $header=header_paginas($dni,$con); echo($header[0]); ?>
 
         <main>
-            <p id="list_header">
-                <b>Consulta ficha del paciente:</b> Introduzca N° historia / paciente / código
-            </p>
-            <div class="data_field" style="grid-column: 1/3">
-                <p class="data_field_title">DNI</p>
-                <p class="data_field_info">12345678</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">Apellidos</p>
-                <p class="data_field_info">Velazquez Vallejos</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">Nombres</p>
-                <p class="data_field_info">Andres Ariel</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">F. Nacimiento</p>
-                <p class="data_field_info">15/04/2000</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">Sexo</p>
-                <p class="data_field_info">Masculino</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">Email</p>
-                <p class="data_field_info">VelazquezAndres@gmail.com</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">N° S. social</p>
-                <p class="data_field_info">28 12345678 40</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">Provincia</p>
-                <p class="data_field_info">Buenos Aires</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">Localidad</p>
-                <p class="data_field_info">Ramos Mejía</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">Dirección</p>
-                <p class="data_field_info">9 de Julio, 1928</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">C. Postal</p>
-                <p class="data_field_info">1704</p>
-            </div>
-            <div class="horizontal_line"></div>
-            <div class="data_field">
-                <p class="data_field_title">F. Consulta</p>
-                <p class="data_field_info">19/08/2021</p>
-            </div>
-            <div class="data_field">
-                <p class="data_field_title">F. Alta</p>
-                <p class="data_field_info">5/09/2021</p>
-            </div>
-            <div class="data_field" style="grid-column: 1/3">
-                <p class="data_field_title">Duración Proceso</p>
-                <p class="data_field_info">17 días</p>
-            </div>
-            <div class="big_data_field">
-                <p class="data_field_title">Diagnóstico</p>
-                <p class="big_data_field_info">Texto</p>
-            </div>
-            <div class="big_data_field">
-                <p class="data_field_title">Tratamiento</p>
-                <p class="big_data_field_info">Texto</p>
-            </div>
-            <div class="big_data_field">
-                <p class="data_field_title">Otros datos de interés</p>
-                <p class="big_data_field_info">Texto</p>
-            </div>
-            <div class="big_data_field">
-                <p class="data_field_title">Observaciones | Notas</p>
-                <p class="big_data_field_info">Texto</p>
-            </div>
+            <form method="post">
+                <p id="list_header">
+                    <b>Consulta ficha del paciente:</b> Introduzca N° historia / paciente / código
+                </p>
+                <?php 
+                    $varsesion= $_SESSION['dni_logeado'];
+                    $consulta="SELECT * FROM `usuario` WHERE dni=$varsesion;";
+                    $sel = $con -> query($consulta);
+                    while($fila = $sel -> fetch_assoc()){ ?>
+
+                        <div class="data_field" style="grid-column: 1/3">
+                            <p class="data_field_title">DNI</p>
+                            <input type="text" value="<?php echo($fila['dni']); ?>" class="data_field_info" disabled>
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">Apellidos</p>
+                            <input type="text" name="apellido" value="<?php echo($fila['apellido']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">Nombres</p>
+                            <input type="text" name="nombre" value="<?php echo($fila['nombre']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">F. Nacimiento</p>
+                            <input type="date" name="f_nacimiento" value="<?php echo($fila['f_nacimiento']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">Sexo</p>
+                            <select name="sexo" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                                <?php if($fila['sexo']=="Masculino"){
+
+                                    echo("<option value='Masculino' selected>Masculino</option>
+                                         <option value='Femenino'>Femenino</option>");
+
+                                }elseif($fila['sexo']=="Femenino"){
+
+                                    echo("<option value='Masculino'>Masculino</option>
+                                         <option value='Femenino' selected>Femenino</option>");
+
+                                }else{
+                                    echo("<option value='Masculino'>Masculino</option>
+                                          <option value='Femenino'>Femenino</option>");
+                                }
+                                
+                                ?>
+                            </select>
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">Email</p>
+                            <input type="email" name="email" value="<?php echo($fila['correo']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">N° S. social</p>
+                            <input type="number" name="n_s_social" value="<?php echo($fila['n_s_social']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                <?php } ?>
+
+                <?php 
+                    $varsesion= $_SESSION['dni_logeado'];
+                    $c_direccion="SELECT * FROM `u_direccion` WHERE dni_dir=$varsesion;";
+                    $sel = $con -> query($c_direccion);
+                    while($fila = $sel -> fetch_assoc()){ ?>
+                    
+                        <div class="data_field">
+                            <p class="data_field_title">Dirección</p>
+                            <input type="text" name="direccion" value="<?php echo($fila['calle_numero']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">Localidad</p>
+                            <input type="text" name="localidad" value="<?php echo($fila['localidad']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">C. Postal</p>
+                            <input type="number" name="c_postal" value="<?php echo($fila['codigo_postal']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                        <div class="data_field">
+                            <p class="data_field_title">Provincia</p>
+                            <input type="text" name="provincia" value="<?php echo($fila['provincia']); ?>" class="data_field_info" <?php if(empty($editar) || $editar==''){echo("disabled");} ?> >
+                        </div>
+                <?php } ?>
+                <div class="data_field">
+                    <input type="submit" name="edicion" value="Editar" id="submit_edit_btn">
+                    <input type="submit" name="guardado" value="Guardar" id="submit_save_btn" <?php if(empty($editar) || $editar==''){echo("disabled");} ?>>
+                </div>
+                <div class="horizontal_line"></div>
+                <div class="data_field">
+                    <p class="data_field_title">F. Consulta</p>
+                    <p class="data_field_info">19/08/2021</p>
+                </div>
+                <div class="data_field">
+                    <p class="data_field_title">F. Alta</p>
+                    <p class="data_field_info">5/09/2021</p>
+                </div>
+                <div class="data_field" style="grid-column: 1/3">
+                    <p class="data_field_title">Duración Proceso</p>
+                    <p class="data_field_info">17 días</p>
+                </div>
+                <div class="big_data_field">
+                    <p class="data_field_title">Diagnóstico</p>
+                    <p class="big_data_field_info">Texto</p>
+                </div>
+                <div class="big_data_field">
+                    <p class="data_field_title">Tratamiento</p>
+                    <p class="big_data_field_info">Texto</p>
+                </div>
+                <div class="big_data_field">
+                    <p class="data_field_title">Otros datos de interés</p>
+                    <p class="big_data_field_info">Texto</p>
+                </div>
+                <div class="big_data_field">
+                    <p class="data_field_title">Observaciones | Notas</p>
+                    <p class="big_data_field_info">Texto</p>
+                </div>
+            </form>
         </main>
 
         <footer></footer>
